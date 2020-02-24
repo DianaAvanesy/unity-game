ceb57694cfe48729d778eb06b55ed66d0001ce75
DianaBanana <dianaskybin@gmail.com>
2020-02-24 16:01:32 -0500

    initial commit

---
+++ Assets/collishion_behavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collishion_behavior : MonoBehaviour
+{
+    // this will run when colision occurs with a trigder
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 scale = new Vector3(3, 3, 3);
+        other.gameObject.transform.localScale = scale;
+    }
+}
